d0a04af7352ac0ae36a82f80028c949f9665b767
AnkitSharma030 <sharmaankit24009@gmail.com>
2025-06-17 22:59:20 +0530

    Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,12 +9,12 @@ const Projects = () => {
         <div className="project-card">
           <h3>Weather Forecasting Website</h3>
           <p>Built a dynamic weather site with real-time updates and interactive UI..</p>
-          <a href="https://github.com/AnkitSharma030" target="_blank">View on GitHub</a>
+          <a href="https://github.com/AnkitSharma030" target="_blank" rel='noreferrer'>View on GitHub</a>
         </div>
         <div className="project-card">
           <h3>Blog Website</h3>
           <p>Created an engaging blog platform with community features and personalized content.</p>
-          <a href="https://github.com/AnkitSharma030" target="_blank">View on GitHub</a>
+          <a href="https://github.com/AnkitSharma030" target="_blank" rel='noreferrer'>View on GitHub</a>
         </div>
       </div>
     </section>
